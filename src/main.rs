55ed3f00c37a8b9c1ef236955dd07836cf0aad61
Juan Lledo <juan.lledo@hotmail.com>
2024-05-01 21:21:49 +0200

    test: add test for more mov types

--- src/main.rs
+++ src/main.rs
@@ -117,4 +117,37 @@ mov bp, ax
             .to_string()
         )
     }
+
+    #[test]
+    fn more_movs() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("listings/listing_0039_more_movs");
+        let instructions = std::fs::read(path).unwrap();
+
+        let dissassembly = disassemble(&instructions);
+
+        assert_eq!(
+            dissassembly,
+            "bits 16
+
+mov si, bx
+mov dh, al
+mov cl, 12
+mov ch, -12
+mov cx, 12
+mov cx, -12
+mov dx, 3948
+mov dx, -3948
+mov al, [bx + si]
+mov bx, [bp + di]
+mov dx, [bp]
+mov ah, [bx + si + 4]
+mov al, [bx + si + 4999]
+mov [bx + di], cx
+mov [bp + si], cl
+mov [bp], ch
+"
+            .to_string()
+        )
+    }
 }
