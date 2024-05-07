67ff505dbc011f1bc9ca74876a974276835bd42e
Juan Lledo <juan.lledo@hotmail.com>
2024-05-07 22:25:12 +0200

    Revert "test: add positive 8 bit immediate to register test"
    This reverts commit ff6df4d9503af1d8ece9bbe7ebeb764b7b784412.

--- src/mov.rs
+++ src/mov.rs
@@ -128,12 +128,4 @@ mod tests {
         let operation = MovOperation::new(instruction_first_byte, instruction_second_byte);
         assert_eq!(operation.to_string(), "mov dh, al".to_string());
     }
-
-    #[test]
-    fn immediate_to_register_8_bit_positive() {
-        let instruction_first_byte = 0b1011_0001;
-        let instruction_second_byte = 0b0000_1100;
-        let operation = MovOperation::new(instruction_first_byte, instruction_second_byte);
-        assert_eq!(operation.to_string(), "mov cl, 12");
-    }
 }
